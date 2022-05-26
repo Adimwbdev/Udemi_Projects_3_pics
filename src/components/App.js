b813178c44533cc08a1bb77b77e7471a4ddaf9cb
adimtheworst <adim.vort@gmail.com>
2022-05-26 20:01:19 +0300

    Fetch Axis

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+        params: {query: term },
+        headers: {
+         Authorization: 'Client-ID VtVD0ZugptbaChNsxwhEIsUusrF_hsc0qNEdnZgPkRM'   
+             }
+        });
+    }
+
+    render() {
+    return (<div className="ui container" style={{marginTop: '20px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        </div>
+    );      
+    }
+}
+
+export default App;
+
+
